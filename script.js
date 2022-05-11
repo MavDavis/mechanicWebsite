6af1fc06c296842cc01022d541e02a4ef98f4956
MavDavis <ekwesimarvelous@gmail.com>
2022-05-11 08:34:42 +0100

    commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+AOS.init();
 
-  AOS.init();
+let hamburger = document.getElementById("hamburger");
+hamburger.addEventListener("click", displayNavbar);
 
-  let hamburger = document.getElementById('hamburger');
-  hamburger.addEventListener('click', displayNavbar);
-  function displayNavbar(){
-      let header = document.getElementById('header');
-      let navbar = document.getElementById('navbar');
-      header.classList.toggle('displayHeader');
-     navbar.classList.toggle('displayNavbar')
-  }
+function displayNavbar() {
+    let header = document.getElementById("header");
+    let navbar = document.getElementById("navbar");
+    header.classList.toggle("displayHeader");
+    navbar.classList.toggle("displayNavbar");
+}
